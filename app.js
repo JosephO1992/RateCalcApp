adc8ef160459e9bf9c38b027d90454954460e8bc
JosephO1992 <josephraoreilly@gmail.com>
2020-09-15 22:20:44 +0800

    Changed output to an input box.value. Changed the % function.

--- app.js
+++ app.js
@@ -1,64 +1,63 @@
-const inputRate = document.querySelector('input.rate');
-const inputOnCosts = document.querySelector('input.onCosts');
-const inputMargin = document.querySelector('input.margin');
-const calculateButton = document.querySelector('button.calculate');
-const form = document.querySelector('form');
-const outPut = document.querySelector('.answer');
-const incSuper = document.querySelector('.inc');
-const exclSuper = document.querySelector('.excl');
-const checkBoxArr = [incSuper, exclSuper];
+const inputRate = document.querySelector("input.rate");
+const inputOnCosts = document.querySelector("input.onCosts");
+const inputMargin = document.querySelector("input.margin");
+const calculateButton = document.querySelector("button.calculate");
+const form = document.querySelector("form");
+const outPut = document.querySelector(".answer");
+// const incSuper = document.querySelector(".inc");
+// const exclSuper = document.querySelector(".excl");
+// const checkBoxArr = [incSuper, exclSuper];
 
-function convertToNum(inputString) { // This function takes the inputs and converts them to numbers as they are strings by default
-    let strV = inputString.value;
-    let numV = parseInt(strV);
-    return numV;
+let numInputRate;
+let numInputOnCosts;
+let numInputMargin;
+let percentOncosts;
+let finalDayRate;
+
+function convertToNum(inputString) {
+	// This function takes the inputs and converts them to numbers as they are strings by default
+	let strV = inputString.value;
+	let numV = parseInt(strV);
+	return numV;
 }
 
- function convertToPercentage(Oncosts) { // This takes in the oncosts input and converts to a %
-    percent = 1 + '.' + Oncosts;
-    numPercent = parseFloat(percent);
-    return numPercent;
- };
+function convertToPercentage(Oncosts) {
+	// This takes in the oncosts input and converts to a %
+	percent = Oncosts / 100;
+	numPercent = parseFloat(percent);
+	return numPercent;
+}
 
- function totalDayRate(rate, percentage, margin) { // This function takes in the aleady converted inputs, then multiplies and returns the total figure   
-        let totalRate = (rate * percentage) + margin;
-        return totalRate;
- }
+function totalDayRate(rate, percentage, margin) {
+	// This function takes in the aleady converted inputs, then multiplies and returns the total figure
+	// I will wrap this in an IF to make sure the % on costs are less than 100
+	let finalPercent = percentage + 1;
 
- function checkboxlimit(checkgroup, limit) {
-     let checkGroupToCheck = checkgroup;
-     let checkBoxLimit = limit;
-     for (let i = 0; i < checkGroupToCheck.length; i++) {
-        let count = 0; 
-        if(checkGroupToCheck[i].checked) {
-            count += 1;
-            if(count > checkBoxLimit) {
-                console.log("too many checkboxes ticked");
-            }
-        }
-     }
- }
- 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // loop through the checkboxes to make sure they both aren't ticked
-    // checkboxlimit(checkBoxArr, 1);
-    if(inputRate == "" && inputOnCosts == "" && inputMargin == "" ){ // making sure there are no empty boxes
-        let numInputRate = convertToNum(inputRate);
-        let numInputOnCosts = convertToNum(inputOnCosts);
-        let numInputMargin = convertToNum(inputMargin);
-        let percentOncosts = convertToPercentage(numInputOnCosts);
-        let finalDayRate = totalDayRate(numInputRate, percentOncosts, numInputMargin);
-        outPut.textContent = finalDayRate; 
-    } else {
-        alert('please make sure you fill out all the boxes');
-    }
- 
+	let totalRate = rate * finalPercent + margin;
+	return totalRate;
+}
+
+function checkboxlimit(checkgroup, limit) {
+	let checkGroupToCheck = checkgroup;
+	let checkBoxLimit = limit;
+	for (let i = 0; i < checkGroupToCheck.length; i++) {
+		let count = 0;
+		if (checkGroupToCheck[i].checked) {
+			count += 1;
+			if (count > checkBoxLimit) {
+				console.log("too many checkboxes ticked");
+			}
+		}
+	}
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	let numInputRate = convertToNum(inputRate);
+	let numInputOnCosts = convertToNum(inputOnCosts);
+	let numInputMargin = convertToNum(inputMargin);
+	let percentOncosts = convertToPercentage(numInputOnCosts);
+	let finalDayRate = totalDayRate(numInputRate, percentOncosts, numInputMargin);
+	outPut.value = finalDayRate;
 });
-
-
-
-
-
-
-
--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
-# Creating a rate calculator app 
+# Creating a rate calculator app
 
 This will be able to perform some simple math within a form
+
+I have created some simple input fields where I can put in some figures and it will pass back out a rate into the input box.
+
+There are few functions to convert the inputs into numbers, as the original value returned is a string, which I cannot perform math with.
+
+I also need to convert back the % figure inputted into an actual percent. For this I just divided that by 100. In the future, I would create
+some test to make sure the % input can't go over 99%.
+
+Originally, there was a an empty div, but I adjusted this to change the value of the text input box to the final value.
+I chose to do this to keep the styling consistent and not having to worry about creating more divs on the page.
